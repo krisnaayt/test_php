fbf19aa7ed889eeb7c006645636971773191f376
krisnaayt <krisnaadiy@gmail.com>
2021-05-21 09:01:47 +0800

    update error SeederUser

--- database/seeds/SeederUser.php
+++ database/seeds/SeederUser.php
@@ -32,10 +32,10 @@ class SeederUser extends Seeder
             ('1', 'admin_surat', 'pa.batulicin@gmail.com', '$2y$10\$eyhZ1SUoIox1ad5ecgtQZO4bICf9pbjPZXnnna\/QNrYqCYqty5FrC', 'admin_surat', 'Admin Surat', '', '', ''),
             ('2', 'admin_emus', 'pa.batulicin@gmail.com', '$2y$10\$eyhZ1SUoIox1ad5ecgtQZO4bICf9pbjPZXnnna\/QNrYqCYqty5FrC', 'admin_emus', 'Admin Emus', '', '', ''),
             ('3', 'panitera', 'pa.batulicin@gmail.com', '$2y$10\$eyhZ1SUoIox1ad5ecgtQZO4bICf9pbjPZXnnna\/QNrYqCYqty5FrC', 'admin_emus', 'H. Yahyadi, S.H.', '197301122003121002', 'Panitera', 'panitera'),
-            ('4', 'panmud_permohonan', 'pa.batulicin@gmail.com', '$2y$10\$eyhZ1SUoIox1ad5ecgtQZO4bICf9pbjPZXnnna\/QNrYqCYqty5FrC', 'admin_emus', 'Khomsiatun Maisaroh, S.H.', '198305042009042011', 'Panitera Muda Permohonan', 'panmud_permohonan),
+            ('4', 'panmud_permohonan', 'pa.batulicin@gmail.com', '$2y$10\$eyhZ1SUoIox1ad5ecgtQZO4bICf9pbjPZXnnna\/QNrYqCYqty5FrC', 'admin_emus', 'Khomsiatun Maisaroh, S.H.', '198305042009042011', 'Panitera Muda Permohonan', 'panmud_permohonan'),
             ('5', 'panmud_gugatan', 'pa.batulicin@gmail.com', '$2y$10\$eyhZ1SUoIox1ad5ecgtQZO4bICf9pbjPZXnnna\/QNrYqCYqty5FrC', 'admin_emus', 'M. Kharis Ridhani, S.H., M.H.', '198709152006041002', 'Panitera Muda Gugatan', 'panmud_gugatan'),
-            ('6', 'panitera_pengganti', 'pa.batulicin@gmail.com', '$2y$10\$eyhZ1SUoIox1ad5ecgtQZO4bICf9pbjPZXnnna\/QNrYqCYqty5FrC', 'admin_emus', 'Muzdalifah, S.H.I', '198905172014032004', 'Panitera Pengganti', panitera_pengganti),
-            ('7', 'panmud_hukum', 'pa.batulicin@gmail.com', '$2y$10\$eyhZ1SUoIox1ad5ecgtQZO4bICf9pbjPZXnnna\/QNrYqCYqty5FrC', 'admin_emus', 'Panitera Muda Hukum', '', 'Panitera Muda Hukum', panmud_hukum)
+            ('6', 'panitera_pengganti', 'pa.batulicin@gmail.com', '$2y$10\$eyhZ1SUoIox1ad5ecgtQZO4bICf9pbjPZXnnna\/QNrYqCYqty5FrC', 'admin_emus', 'Muzdalifah, S.H.I', '198905172014032004', 'Panitera Pengganti', 'panitera_pengganti'),
+            ('7', 'panmud_hukum', 'pa.batulicin@gmail.com', '$2y$10\$eyhZ1SUoIox1ad5ecgtQZO4bICf9pbjPZXnnna\/QNrYqCYqty5FrC', 'admin_emus', 'Panitera Muda Hukum', '', 'Panitera Muda Hukum', 'panmud_hukum')
             ;
         ");
     }
